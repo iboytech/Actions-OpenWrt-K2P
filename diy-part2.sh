156a691ac480a0edb2db586d984164603a745c4b
iboy <87509171+iboytech@users.noreply.github.com>
2023-01-23 21:02:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,13 +10,31 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# 1.修改默认ip
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# # Install theme
-# rm -rf package/lean/luci-theme-argon 
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+# 2.修改主机名
+# sed -i 's/OpenWrt/K2P/g' package/base-files/files/bin/config_generate
+
+# 4.修改版本号
+sed -i "s/OpenWrt /0012h build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# 6.设置ttyd免登录
+sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
+
+# Install theme
+rm -rf package/lean/luci-theme-*
+git clone -b master https://github.com/kiddin9/luci-theme-edge.git  package/lean/luci-theme-edge
 # git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-theme-argon-config
 
+# 5.修改默认主题
+sed -i ' s/luci-theme-bootstrap/luci-theme-edge/g ' feeds/luci/collections/luci/Makefile
+
+# 7.修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
 # # Change opkg source
-# echo "sed -i 's#https://mirrors.cloud.tencent.com/lede/snapshots#https://k2p.dreamwalkerxz.workers.dev/k2p-repo#g' /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
+echo "sed -i 's#https://mirrors.cloud.tencent.com/lede/snapshots#https://k2p.dreamwalkerxz.workers.dev/k2p-repo#g' /etc/opkg/distfeeds.conf" >> package/lean/default-settings/files/zzz-default-settings
+
+
+
